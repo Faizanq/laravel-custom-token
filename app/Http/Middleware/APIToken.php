9d478407a86291545b636429982ba5773c2fe053
faizanq <faizanqureshi0@gmail.com>
2018-12-07 09:04:08 +0530

    API Token added

---
+++ app/Http/Middleware/APIToken.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class APIToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next){
+
+          if($request->header('Authorization')){
+            return $next($request);
+          }
+
+           $response['error'][] = 'Unauthorized';  
+           $response['data'] = [];  
+           $response['success'] = 0; 
+           return response()->json($response, $code);
+    }
+}
